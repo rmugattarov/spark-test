e5713dfaf40858349d410035742ddb92292091bc
rmugattarov <mugattarov@gmail.com>
2016-10-21 19:07:20 +0300

    updates

--- src/main/java/rmugattarov/spark_test/HelloWorld.java
+++ src/main/java/rmugattarov/spark_test/HelloWorld.java
@@ -1,12 +1,14 @@
 package rmugattarov.spark_test;
+import spark.route.RouteOverview;
+
 import static spark.Spark.*;
 /**
  * Created by rmugattarov on 21.10.2016.
  */
 public class HelloWorld {
     public static void main(String[] args) {
+        RouteOverview.enableRouteOverview();
         get("/hello/:name", (req, res) -> {
-            System.out.println("Bazinga!");
             return String.format("Hello %s!", req.params(":name"));
         });
     }
