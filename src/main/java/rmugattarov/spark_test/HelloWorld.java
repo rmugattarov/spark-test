86ea8af766e20c4c89199f972e2d2a6be06d8bf5
rmugattarov <mugattarov@gmail.com>
2016-10-22 16:16:01 +0300

    updates

--- src/main/java/rmugattarov/spark_test/HelloWorld.java
+++ src/main/java/rmugattarov/spark_test/HelloWorld.java
@@ -15,12 +15,18 @@ import static spark.Spark.*;
 public class HelloWorld {
     private static Map<String, Object> model = new HashMap<>();
     private static ModelAndView modelAndView = new ModelAndView(model, "template.vm");
+    private static String[] arr = new String[3];
 
     public static void main(String[] args) {
         Spark.staticFileLocation("/public");
         model.put("msg", "whaddup");
+
         get("/hello", (req, res) -> {
             model.put("msg", String.valueOf(Math.random()));
+            for(int i=0; i <3; i++) {
+                arr[i] = String.valueOf(Math.random());
+            }
+            model.put("arr", arr);
             return modelAndView;
         }, new VelocityTemplateEngine());
     }
